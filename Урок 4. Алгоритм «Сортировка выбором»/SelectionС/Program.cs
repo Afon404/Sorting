e23be78e2499f0a2c1c3de8c8a6573e1e9aa9cce
Afon404 <112636609+Afon404@users.noreply.github.com>
2023-02-24 15:39:11 +0300

    Add files via upload

---
+++ Урок 4. Алгоритм «Сортировка выбором»/SelectionС/Program.cs
@@ -0,0 +1,31 @@
+﻿// using static Sorting;
+
+// // using static System.Console;  Если добавить эту строку то можно тогда убрать из строк System;  
+
+//  int[] array = {22, 34, 5, 6, 77, 85, 2, 4, 5, 3, 2, 9};    // Создаём массив
+
+// System.Console.WriteLine($"[{String.Join(" ,", array)}]"); //Показываем массив
+// SortSelection(array);
+// System.Console.WriteLine($"[{String.Join(" ,", array)}]");  // Сортируем массив
+
+
+////////////////Способ 2:
+
+using static Sorting;
+using static Infrastructure;
+
+10.CreateArray(min: 10, max: 30)
+  .Show("| ")
+  .SortSelection()
+  .Show("}, {");
+Console.ReadLine();
+
+
+
+  /////Способ 3 
+
+
+// int[] ar = 10.CreateArray(min: 10, max: 30);   //  Заполняем массив
+// ar.Show();                                    // Показываем его
+// ar.SortSelection();                         //  Сотрируем в Файле Sorting
+// ar.Show();                                  // И показываем отсортировачный массив
